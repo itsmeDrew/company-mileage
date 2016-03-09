57de423769eae26c22b5d7368d9f2d8766a62a1b
Drew Roberts <drew.roberts@methodfactory.com>
2016-03-08 20:22:42 -0500

    begin header styling

--- src/header.php
+++ src/header.php
@@ -10,18 +10,14 @@
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-  <div id="page" class="hfeed site">
-    <div id="wrap-header" class="wrap-header">
-      <header id="masthead" class="site-header">
-        <div class="site-branding">
-          <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-          <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-          <img src="<?php bloginfo(template_url); ?>/img/logo.png" alt="" />
-        </div>
-        <button id="responsive-menu-toggle"><?php _e( 'Menu', 'voidx' ); ?></button>
-        <nav id="site-navigation" class="site-navigation">
-          <div id="responsive-menu"><?php wp_nav_menu( array( 'theme_location' => 'header', 'menu_id' => 'menu-header', 'menu_class' => 'menu-inline' ) ); ?></div>
-        </nav>
-      </header>
+  <header class="site-header">
+    <div class="site-branding">
+      <img class="site-branding__logo" src="<?php bloginfo(template_url); ?>/img/logo.png" alt="" />
     </div>
-    <div id="wrap-main" class="wrap-main">
+    <nav class="site-nav">
+      <a class="site-nav__link" href="#">Link</a>
+      <a class="site-nav__link" href="#">Link</a>
+      <a class="site-nav__link" href="#">Link</a>
+    </nav>
+  </header>
+<div id="wrap-main" class="wrap-main">
