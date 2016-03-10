3b6b4d1de50ce7641148f224e078ea3211cd29b1
Drew Roberts <drewr@admins-MacBook-Pro-5.local>
2016-03-10 17:14:32 -0500

    Created home tpl

---
+++ src/templates/home-tpl.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template Name: Home
+ *
+ * @package Company Mileage
+ * @subpackage company-mileage
+ * @since Company Mileage 1.0.1
+ */
+get_header();
+?>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+    <?php the_content(); ?>
+
+  <?php endwhile; else : ?>
+	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
