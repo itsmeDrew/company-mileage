2a6dc7d24c31e67a9e45f9629ba7acca29b3f81b
Drew Roberts <drew.roberts@methodfactory.com>
2016-03-12 15:12:36 -0500

    implemented bootstrap / revised nav

--- build/header.php
+++ build/header.php
@@ -7,39 +7,59 @@
 <title><?php wp_title( '-', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+
+<!-- bootstrap -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous"> -->
+
+
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+
   <header class="site-header">
-    <div class="site-header__content">
-      <div class="site-branding">
-        <img class="site-branding__logo" src="<?php bloginfo('template_url'); ?>/img/logo.png" alt="" />
-      </div>
-      <div class="site-nav--wrapper">
-        <div class="site-login hidden--sm">
-          <a class="btn-primary site-login__btn" href="#">request demo</a>
-          <a class="btn-secondary site-login__btn" href="#">login</a>
+    <nav class="js-sticky-nav navbar navbar-default">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a href="<?php echo get_permalink(10); ?>"><img class="navbar__logo" src="<?php bloginfo('template_url'); ?>/img/logo.png" alt="logo" /></a>
         </div>
 
-        <!-- START NAV -->
-        <nav class="site-nav">
-          <i class="fa fa-user hidden--lg"></i>
-          <ul class="site-nav__list hidden--sm">
-            <?php
-              $sitePages = array(
-                'depth'        => 2,
-                'exclude'      => '',
-                'post_status'  => 'publish',
-                'sort_column'  => 'menu_order',
-                'title_li'     => ''
-              );
-            ?>
-            <?php wp_list_pages($sitePages); ?>
-          </ul>
-        </nav>
-        <!-- END NAV -->
-      </div>
-    </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <div class="navbar-right">
+            <div class="navbar-top">
+              <span class="site-login__phone">Call (877) 677 0377</span>
+              <a class="btn-primary site-login__btn" href="#">request demo</a>
+              <a class="btn-secondary site-login__btn" href="#">request demo</a>
+            </div>
+            <ul class="nav navbar-nav">
+              <li class="page_item navbar-nav__cart">
+                <ul class="nav navbar-nav">
+                  <?php
+                    $sitePages = array(
+                      'depth'        => 2,
+                      'exclude'      => '',
+                      'post_status'  => 'publish',
+                      'sort_column'  => 'menu_order',
+                      'title_li'     => ''
+                    );
+                  ?>
+                  <?php wp_list_pages($sitePages); ?>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
 
   </header>
 <div id="wrap-main" class="wrap-main">
--- build/templates/home-tpl.php
+++ build/templates/home-tpl.php
@@ -9,6 +9,15 @@
 get_header();
 ?>
 
+<div class="jumbotron" role="hero">
+  <div class="container">
+    <h2 class="jumbotron__heading">Save 20-30% on Mileage COSTS</h2>
+    <p class="jumbotron__desc">
+      CompanyMileage provides companies with significant savings on mileage reimbursement by accurately verifying employee mileage & automating the reimbursement process. How much could your company save?
+    </p>
+  </div>
+</div>
+
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
     <?php the_content(); ?>
