e88b3269717e2847197a2dc0d5661f101255c29f
Drew Roberts <drew.roberts@methodfactory.com>
2016-03-13 16:55:55 -0400

    home solutions / about build

--- build/templates/home-tpl.php
+++ build/templates/home-tpl.php
@@ -26,10 +26,13 @@ get_header();
     </div>
   </div>
 </div>
+
+<!-- /// START ABOUT /// -->
+
 <div class="about">
   <div class="container">
     <header class="about-header">
-      <h2 class="about__heading">Trusted nationwide by the distributted workforce:</h2>
+      <h2 class="about-header__heading">Trusted nationwide by the distributted workforce:</h2>
       <ul class="about-cases__list">
         <li class="about-cases__list-item col-sm-3">
           <img class="about-cases__img" src="<?php bloginfo('template_url'); ?>/img/case-cleveland-clinic.png" alt="cleveland clinic" />
@@ -44,26 +47,64 @@ get_header();
           <img class="about-cases__img" src="<?php bloginfo('template_url'); ?>/img/case-cleveland-clinic.png" alt="cleveland clinic" />
         </li>
       </ul>
-      <div class="about-info col-md-6">
-        <h2 class="about-info__heading">An Employer System for Mileage Reimbursement that Saves Time & Money</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </header>
+    <div class="about-info col-md-6">
+      <h2 class="about-info__heading">An Employer System for Mileage Reimbursement that Saves Time & Money</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </div>
+    <div class="about-demo col-md-6">
+      <div class="demo">
+        <h2 class="demo__heading">View a Demo</h2>
+        <p class="demo__p">
+          In just 2 minutes, you can see how easily CompanyMileage can automate your mileage reimbursement processes.
         </p>
-      </div>
-      <div class="about-demo col-md-6">
-        <div class="demo">
-          <h2 class="demo__heading">View a Demo</h2>
-          <p class="demo__p">
-            In just 2 minutes, you can see how easily CompanyMileage can automate your mileage reimbursement processes.
-          </p>
-          <div class="demo-form">
-            <input class="demo-form__input" type="text" name="email" placeholder="Your Email Address">
-            <a class="demo-form__btn btn-primary" href="#">watch demo</a>
-          </div>
+        <div class="demo-form">
+          <input class="demo-form__input" type="text" name="email" placeholder="Your Email Address">
+          <a class="demo-form__btn btn-primary" href="#">watch demo</a>
         </div>
       </div>
-    </header>
-  </div>
+    </div>
+  </div> <!-- end container -->
 </div>
 
+<!-- /// END ABOUT /// -->
+
+<div class="solutions">
+  <div class="container">
+    <header class="solutions-header">
+      <h2 class="solutions-header__heading">A Suite of Solutions Improving the Reimbursement Process</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </header>
+    <ul class="solutions__list">
+      <li class="solutions__list-item col-md-4">
+        <img src="<?php bloginfo('template_url'); ?>/img/ico-solutions-key.png" alt="key" />
+        <h2>SureMileage</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.
+        </p>
+        <a class="solutions__btn btn-secondary" href="#">learn more</a>
+      </li>
+      <li class="solutions__list-item col-md-4">
+        <img src="<?php bloginfo('template_url'); ?>/img/ico-solutions-key.png" alt="key" />
+        <h2>SureExpense</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.
+        </p>
+        <a class="solutions__btn btn-secondary" href="#">learn more</a>
+      </li>
+      <li class="solutions__list-item col-md-4">
+        <img src="<?php bloginfo('template_url'); ?>/img/ico-solutions-key.png" alt="key" />
+        <h2>SureMobile</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.
+        </p>
+        <a class="solutions__btn btn-secondary" href="#">learn more</a>
+      </li>
+    </ul>
+  </div>
+</div>
 <?php get_footer(); ?>
