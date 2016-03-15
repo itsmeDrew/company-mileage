019ddec5ca47ff25c68208c45fb1b895b1fc1cf6
Drew Roberts <drew.roberts@methodfactory.com>
2016-03-14 22:41:46 -0400

    footer top build

--- build/footer.php
+++ build/footer.php
@@ -3,7 +3,7 @@
 
         <div class="footer-top">
           <div class="container">
-            <div class="company col-md-6">
+            <div class="company col-sm-6">
               <img class="company-info__img" src="<?php bloginfo('template_url'); ?>/img/ico-company.png" alt="company" />
               <div class="company-info">
                 <span class="company-info__company">CompanyMileage LLC</span>
@@ -14,7 +14,7 @@
                 </span>
               </div>
             </div>
-            <div class="footer-blog col-md-6">
+            <div class="footer-blog col-sm-6">
               <h2 class="footer-blog__heading">recent blogs:</h2>
               <ul class="footer-blog__list">
                 <li class="footer-blog__list-item">
