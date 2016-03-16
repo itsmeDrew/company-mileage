1b010c8d6b2b7d9dd7994c41783c286f355d8b66
Drew Roberts <drew.roberts@methodfactory.com>
2016-03-15 20:48:33 -0400

    solution jumbotron

--- src/templates/solution-tpl.php
+++ src/templates/solution-tpl.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Home
+ * Template Name: Solution
  *
  * @package Company Mileage
  * @subpackage company-mileage
@@ -11,19 +11,15 @@ get_header();
 
 <div class="jumbotron">
   <div class="container">
-    <h1 class="jumbotron__heading">Save 20-30% on Mileage COSTS</h1>
+  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <h1 class="jumbotron__heading"><?php the_title(); ?></h1>
     <p class="jumbotron__desc">
-      CompanyMileage provides companies with significant savings on mileage reimbursement by accurately verifying employee mileage & automating the reimbursement process. How much could your company save?
+      <?php the_content(); ?>
     </p>
-    <div class="savings-calculator">
-      <input class="savings-calculator__input" type="text" name="name" placeholder="number of employees">
-      <input class="savings-calculator__input" type="text" name="name" placeholder="miles driven per week">
-      <input class="savings-calculator__input" type="text" name="name" placeholder="reimbursment rate">
-      <a class="btn-primary" href="#">calculate savings</a>
-    </div>
     <div class="jumbotron-display">
       <img class="jumbotron-display__img" src="<?php bloginfo('template_url'); ?>/img/map.jpg" alt="map" />
     </div>
+    <?php endwhile; endif; wp_reset_postdata(); ?>
   </div>
 </div>
 <div class="about">
