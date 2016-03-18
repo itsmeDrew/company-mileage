410d8950dee03a2f842d75ce5affba466c01543c
Drew Roberts <drew.roberts@methodfactory.com>
2016-03-17 22:45:30 -0400

    company build / styling

---
+++ src/templates/company-tpl.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Template Name: Company
+ *
+ * @package Company Mileage
+ * @subpackage company-mileage
+ * @since Company Mileage 1.0.1
+ */
+get_header();
+?>
+
+<div class="jumbotron">
+  <div class="jumbotron__container container">
+    <h1 class="jumbotron__heading">We’re building better expense management software in Dallas, Texas</h1>
+    <p class="jumbotron__desc">
+      CompanyMileage provides companies with significant savings on mileage reimbursement by accurately verifying employee mileage & automating the reimbursement process. How much could your company save?
+    </p>
+  </div>
+</div>
+<div class="technology">
+  <div class="container">
+    <div class="col-md-6">
+      <img src="<?php bloginfo('template_url'); ?>/img/ico-gauge.png" alt="ico" />
+    </div>
+    <div class="col-md-6">
+      <p>
+        CompanyMileage uses a proprietary, automated web-based technology that accurately verifies employee mileage and manages the entire reimbursement process. Our system provides companies with significant savings on their mileage reimbursement in an accurate, measurable way that is painless to implement.
+      </p>
+    </div>
+  </div>
+</div>
+<div class="team">
+  <div class="container">
+    <header class="team-header">
+      <h2 class="team-header__heading">our executive team</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+      </p>
+    </header>
+    <div class="team-member">
+      <div class="col-md-6">
+        <div class="team-member__img--wrapper">
+          <img class="team-member__img" src="http://lorempixel.com/200/200" alt="team member" />
+        </div>
+      </div>
+      <div class="col-md-6">
+        <p>
+          Kevin's background as a CPA, Payroll Service Founder, and Technology Services leader made the creation of CompanyMileage a natural evolution of his experience in technology-based accounting, payroll, and time and attendance systems. Kevin oversees the building of the Sure Mileage™ System as well as ensuring client service is of top priority.
+        </p>
+        <p>
+          Kevin earned his CPA while working in financial accounting in the early 90's. In 1993 he co-founded Payroll Associates, Inc. and grew an incredible team that became the largest independent payroll-processing provider in the DFW/Metroplex. Payroll Associates provided services to over 1,100 business and 60,000 employees. In 2005, Payroll Associates was sold to Paychoice and became the Western Region Headquarters. Kevin continued as a member of the senior management team until spring 2006.
+        </p>
+        <p>
+          Kevin has been a member of Entrepreneurs Organization (EO) since 2001 and served on the Dallas Board from 2003-2006.
+        </p>
+      </div>
+    </div>
+    <div class="team-member">
+      <div class="col-md-6">
+        <div class="team-member__img--wrapper">
+          <img class="team-member__img" src="http://lorempixel.com/200/200" alt="team member" />
+        </div>
+      </div>
+      <div class="col-md-6">
+        <p>
+          Kevin's background as a CPA, Payroll Service Founder, and Technology Services leader made the creation of CompanyMileage a natural evolution of his experience in technology-based accounting, payroll, and time and attendance systems. Kevin oversees the building of the Sure Mileage™ System as well as ensuring client service is of top priority.
+        </p>
+        <p>
+          Kevin earned his CPA while working in financial accounting in the early 90's. In 1993 he co-founded Payroll Associates, Inc. and grew an incredible team that became the largest independent payroll-processing provider in the DFW/Metroplex. Payroll Associates provided services to over 1,100 business and 60,000 employees. In 2005, Payroll Associates was sold to Paychoice and became the Western Region Headquarters. Kevin continued as a member of the senior management team until spring 2006.
+        </p>
+        <p>
+          Kevin has been a member of Entrepreneurs Organization (EO) since 2001 and served on the Dallas Board from 2003-2006.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+<?php get_footer(); ?>
