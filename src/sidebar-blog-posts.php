446f7dd34224956c3ba21110ee0b973f0103694f
Drew Roberts <drew.roberts@methodfactory.com>
2016-03-19 22:37:28 -0400

    sidebar blog build/style

--- src/sidebar-blog-posts.php
+++ src/sidebar-blog-posts.php
@@ -1,6 +1,13 @@
-<div class="sidebar-blog">
+<div class="sidebar-blog-posts">
+  <div class="post-data">
+    <img class="post-data__img" src="<?php bloginfo('template_url'); ?>/img/author.png" alt="post-author" />
+    <p class="post-data__author">By CompanyMileage Team</p>
+    <p class="post-data__date"><?php the_time('F j, Y'); ?></p>
+  </div>
   <div class="sidebar-subscribe">
-    <h2 class="sidebar-heading">Learn the skills CFOs need to manage their T&E expense more wisely.</h2>
+    <h2 class="sidebar-heading">Get the T&E expense advice that CFOs rely on.</h2>
+    <p>Subscribe to our blog:</p>
+
     <div class="sidebar-form">
       <input class="sidebar-form__input" type="email" name="text" placeholder="Your Name">
       <input class="sidebar-form__input" type="email" name="email" placeholder="Your Email">
