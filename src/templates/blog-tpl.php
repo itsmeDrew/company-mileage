d4169afdae01faee66200fa1382de3da4dc19207
Drew Roberts <drew.roberts@methodfactory.com>
2016-03-20 14:19:10 -0400

    subhead responsive fixes

--- src/templates/blog-tpl.php
+++ src/templates/blog-tpl.php
@@ -12,11 +12,11 @@ get_header();
 <div class="subhead">
   <div class="container">
     <div class="row">
-      <div class="subhead-header col-md-6">
+      <div class="subhead-header col-xs-8 col-md-6">
         <i class="subhead__ico fa fa-rss"></i>
         <h1 class="subhead__heading">news & updates</h1>
       </div>
-      <div class="subhead-social col-md-6">
+      <div class="subhead-social col-xs-4 col-md-6">
         <i class="subhead__ico fa fa-facebook-square"></i>
         <i class="subhead__ico fa fa-twitter-square"></i>
         <i class="subhead__ico fa fa-linkedin-square"></i>
