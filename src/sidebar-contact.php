4b1bc2454d88c1ca0f012f7a6c5da2ae0231dbdd
Drew Roberts <drew.roberts@methodfactory.com>
2016-03-20 22:09:57 -0400

    sidebar contact

---
+++ src/sidebar-contact.php
@@ -0,0 +1,18 @@
+<div class="sidebar-contact">
+  <div class="sidebar-email">
+    <a class="btn-primary--blue" href="mailto:support@companymileage.com">email support</a>
+    <a class="btn-primary--sky" href="mailto:management@companymileage.com">email management</a>
+    <a class="btn-primary--orange" href="mailto:sales@companymileage.com">email sales</a>
+    <a class="btn-primary--grey" href="#">join webex meeting</a>
+  </div>
+  <div class="sidebar-subscribe">
+    <h2 class="sidebar-heading">Get the T&E expense advice that CFOs rely on.</h2>
+    <p>Subscribe to our blog:</p>
+
+    <div class="sidebar-form">
+      <input class="sidebar-form__input" type="email" name="text" placeholder="Your Name">
+      <input class="sidebar-form__input" type="email" name="email" placeholder="Your Email">
+      <a class="sidebar-form__btn btn-primary" href="#">subscribe</a>
+    </div>
+  </div>
+</div>
--- src/templates/contact-tpl.php
+++ src/templates/contact-tpl.php
@@ -13,11 +13,13 @@ get_header();
 
 <div class="contact container">
   <div class="row">
-    <div class="col-md-8">
-      <?php the_content(); ?>
+    <div class="contact-content col-md-8">
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <?php the_content(); ?>
+      <?php endwhile; endif; wp_reset_postdata(); ?>
     </div>
     <div class="col-md-4">
-
+      <?php get_sidebar('contact'); ?>
     </div>
   </div>
 </div>
