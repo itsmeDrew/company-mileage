ff6e2accfd73ec2e0079b554a6747036452a8ddc
Drew Roberts <drew.roberts@methodfactory.com>
2016-03-24 21:38:57 -0400

    demo's header

--- src/templates/demo-tpl.php
+++ src/templates/demo-tpl.php
@@ -11,17 +11,21 @@ get_header();
 
 <div class="jumbotron">
   <div class="container">
-    <div class="col-md-8">
-      <iframe width="100%" height="315" src="https://www.youtube.com/embed/KwyOfqbP8JU" frameborder="0" allowfullscreen></iframe>
+    <div class="row">
+      <div class="col-md-8">
+        <iframe width="100%" height="315" src="https://www.youtube.com/embed/KwyOfqbP8JU" frameborder="0" allowfullscreen></iframe>
+      </div>
+      <div class="col-md-4">
+        <h2 class="jumbotron__heading">Watch More Demos</h2>
+        <a class="btn-primary--light-grey" href="mailto:support@companymileage.com">email support</a>
+        <a class="btn-primary--sky" href="mailto:management@companymileage.com">email management</a>
+        <a class="btn-primary--orange" href="mailto:sales@companymileage.com">email sales</a>
+        <a class="btn-primary--grey" href="#">join webex meeting</a>
+      </div>
     </div>
-    <div class="col-md-4">
-      <h2 class="jumbotron__heading">Watch More Demos</h2>
-      <a class="btn-primary--light-grey" href="mailto:support@companymileage.com">email support</a>
-      <a class="btn-primary--sky" href="mailto:management@companymileage.com">email management</a>
-      <a class="btn-primary--orange" href="mailto:sales@companymileage.com">email sales</a>
-      <a class="btn-primary--grey" href="#">join webex meeting</a>
-    </div>
+    <strong class="jumbotron__heading--secondary">Call (877) 677-0377 or fill out the contact form below.</strong>
   </div>
+
 </div>
 
 <?php get_footer(); ?>
