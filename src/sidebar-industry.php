317825d86df5232d67374aa98fad363c64d80e0c
Drew Roberts <drew.roberts@methodfactory.com>
2016-03-25 22:47:35 -0400

    added form to industry sidebar

--- src/sidebar-industry.php
+++ src/sidebar-industry.php
@@ -2,8 +2,7 @@
   <div class="sidebar-subscribe">
     <h2 class="sidebar-heading">Watch a Demo</h2>
     <div class="sidebar-form">
-      <input class="sidebar-form__input" type="email" name="text" placeholder="Your Name">
-      <a class="sidebar-form__btn btn-primary" href="#">submit</a>
+      <?php echo do_shortcode('[contact-form-7 id="86" title="Industry Sidebar Demo"]'); ?>
     </div>
   </div>
 </div>
