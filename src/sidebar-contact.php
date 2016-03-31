c46665d7af0a3cf8f58e7d75b8c1551d5503c5a0
Drew Roberts <drew.roberts@methodfactory.com>
2016-03-30 21:19:03 -0400

    linked contact webex

--- src/sidebar-contact.php
+++ src/sidebar-contact.php
@@ -3,7 +3,7 @@
     <a class="btn-primary--blue" href="mailto:support@companymileage.com">email support</a>
     <a class="btn-primary--sky" href="mailto:management@companymileage.com">email management</a>
     <a class="btn-primary--orange" href="mailto:sales@companymileage.com">email sales</a>
-    <a class="btn-primary--grey" href="#">join webex meeting</a>
+    <a class="btn-primary--grey" href="https://companymileage.webex.com/mw3000/mywebex/default.do?siteurl=companymileage">join webex meeting</a>
   </div>
   <div class="sidebar-demo">
     <div class="sidebar-form">
